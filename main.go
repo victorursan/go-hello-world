9311d95b75fe2570fd5d8208ece2a0f786f40265
Victor Ursan <victor.ursan@gmail.com>
2017-07-05 19:19:45 +0300

    removed more redundent extra variable

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	h, _ := os.Hostname()
 	fmt.Fprintf(w, "Hi there, I'm served from GO!")
 }
 func main() {
